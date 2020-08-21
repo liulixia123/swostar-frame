068a02d126aa1b7173358888ab5372619bb5f490
lixia <1518545702@qq.com>
2020-08-21 17:31:40 +0800

    update

---
+++ app/Listener/HandsHakeListener.php
@@ -0,0 +1,82 @@
+<?php
+namespace App\Listener;
+use SwoStar\Event\Listener;
+use SwoStar\Server\WebSocket\WebSocketServer;
+class HandsHakeListener extends Listener
+{
+	protected $name = 'ws.hand';
+	public function handler(WebSocketServer $server = null, $request = null, $response = null)
+	{
+		// 权限校验
+		// 没有携带token直接结束
+		$token = $request->header['sec-websocket-protocol'];
+		if (empty($token) || (!$this->check($server, $token, $request->fd))) {
+		$response->end();
+		return false;
+		}
+		// websocket算法
+		$this->handShake($request, $response);
+	}
+	/**
+	* 对连接的客户端进行权限校验，如果说通过就会存入redis中
+	* 
+	* @param WebSocketServer $server
+	* @param string $token jwt的token
+	* @param int $fd 连接的客户端
+	* @return boolean
+	*/
+	protected function check(WebSocketServer $server, $token, $fd)
+	{
+		try {
+			$config = $this->app->make('config');
+			$key = $config->get('server.route.jwt.key');
+			// 对jwt的token进行解析，返回jwt对象
+			$jwt = JWT::decode($token, $key, [$config->get('server.route.jwt.alg')]);
+			// 从jwt中获取信息
+			$userInfo = $jwt->data;
+			// 然后绑定路由的关系
+			$url = $userInfo->service_url;
+			$server->getRedis()->hset($key, $userInfo->uid, \json_encode([
+				'fd' => $fd,
+				'name' => $userInfo->name
+			]));
+			return true;
+		} catch (\Exception $e) {
+			return false;
+		}
+	}
+	/**
+	* websocke的加密算法
+	* 
+	*/
+	protected function handShake($request, $response)
+	{
+		// websocket握手连接算法验证
+		$secWebSocketKey = $request->header['sec-websocket-key'];
+		$patten = '#^[+/0-9A-Za-z]{21}[AQgw]==$#';
+		if (0 === preg_match($patten, $secWebSocketKey) || 16 !== strlen(base64_decode($secWebSocketKey))) {
+			$response->end();
+			return false;
+		}
+		echo $request->header['sec-websocket-key'];
+		$key = base64_encode(sha1(
+		$request->header['sec-websocket-key'] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11',
+		true
+		));
+		$headers = [
+			'Upgrade' => 'websocket',
+			'Connection' => 'Upgrade',
+			'Sec-WebSocket-Accept' => $key,
+			'Sec-WebSocket-Version' => '13',
+		];
+		if (isset($request->header['sec-websocket-protocol'])) {
+			$headers['Sec-WebSocket-Protocol'] = $request->header['sec-websocket-protocol'];
+		}
+		foreach ($headers as $key => $val) {
+			$response->header($key, $val);
+		}
+		$response->status(101);
+		$response->end();
+	}
+}
+?>
--- config/server.php
+++ config/server.php
@@ -1,19 +1,46 @@
 <?php
 return [
-	'http'=>[
-		'host' => '0.0.0.0',
-		'port' => 9000,
-		'swoole' => [
+	"http" => [
+		"host" => "0.0.0.0",
+		"port" => 9000,
+		"tcpable" =>1,
+		"swoole" => [
+			"task_worker_num" => 0,
 		],
-	'tcpable' => 1, // 1为开启， 0 为关闭
-	'rpc' => [
-		'host' => '127.0.0.1',
-		'port' => 8000,
-		'swoole' => [
-			'worker_num' => 1
+		"rpc" => [
+			"host" => "127.0.0.1",
+			"port" => "9501",
+			"swoole_setting" => [
+				"worker_num" => "2"
+				]
+		]
+	],
+	'ws'=>[
+		'host' => '192.168.186.131', //服务监听ip
+		'port' => 9800, //监听端口
+		'tcpable'=>1, //是否开启tcp监听
+		'enable_http' => true, //是否开启http服务
+		'swoole' => [ //swoole配置
+			"task_worker_num" => 0,
+			// 'daemonize' => 0, //是否开启守护进程
+		],
+		"rpc" => [
+			"host" => "127.0.0.1",
+			"port" => "9502",
+			"swoole_setting" => [
+			"worker_num" => "2"
 			]
 		]
 	],
-	// ...
+	'route' => [
+		'server' => [
+			'host' => '192.168.186.131',
+			'port' => 9600,
+		],
+		'jwt' => [
+			'key' => 'swocloud',
+			'alg' => 'HS256'
+		]
+	]
 ];
 ?>
